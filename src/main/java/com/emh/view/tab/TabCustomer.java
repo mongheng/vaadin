da6da5c36b26f25e79174a20c5f52326504cfc6d
e901 <e901@e901-PC>
2017-08-02 18:07:07 +0700

    commit

--- src/main/java/com/emh/view/tab/TabCustomer.java
+++ src/main/java/com/emh/view/tab/TabCustomer.java
@@ -1,5 +1,9 @@
 package com.emh.view.tab;
 
+import org.springframework.context.ApplicationContext;
+
+import com.emh.view.CustomerFormView;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.VerticalLayout;
@@ -8,8 +12,10 @@ public class TabCustomer extends TabSheet {
 
 	private static final long serialVersionUID = 1L;
 
-	public TabCustomer() {
-		super();
+	private ApplicationContext applicationContext;
+	
+	public TabCustomer(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
 		init();
 	}
 	
@@ -17,14 +23,14 @@ public class TabCustomer extends TabSheet {
 		VerticalLayout tab1 = new VerticalLayout();
 		tab1.addComponent(new Label("Test1"));
 		tab1.setMargin(false);
-		tab1.setCaption("Tab1");
-		
-		VerticalLayout tab2 = new VerticalLayout();
-		tab2.addComponent(new Label("Test2"));
-		tab2.setCaption("Tab2");
+		tab1.setCaption("Customers");
+		tab1.setIcon(VaadinIcons.AIRPLANE);
 		
 		addTab(tab1, 0);
-		addTab(tab2, 1);
+		addTab(new CustomerFormView(applicationContext), "Customer", null, 1);
+		
+		
+		setSizeFull();
 		
 	}
 }
