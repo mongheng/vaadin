7fdb338e478d55ec07014601440238be04cc002d
e901 <e901@e901-PC>
2017-08-05 12:04:11 +0700

    commit

--- src/main/java/com/emh/view/tab/TabCustomer.java
+++ src/main/java/com/emh/view/tab/TabCustomer.java
@@ -2,7 +2,6 @@ package com.emh.view.tab;
 
 import org.springframework.context.ApplicationContext;
 
-import com.emh.model.Customer;
 import com.emh.view.CustomerFormView;
 import com.emh.view.CustomersGirdView;
 import com.vaadin.icons.VaadinIcons;
@@ -14,7 +13,7 @@ public class TabCustomer extends TabSheet {
 	private static final long serialVersionUID = 1L;
 
 	private ApplicationContext applicationContext;
-	private Customer customer;
+	private CustomersGirdView customersGirdView;
 	
 	public TabCustomer(ApplicationContext applicationContext) {
 		this.applicationContext = applicationContext;
@@ -23,7 +22,8 @@ public class TabCustomer extends TabSheet {
 	
 	private void init() {
 		VerticalLayout tab1 = new VerticalLayout();
-		tab1.addComponent(new CustomersGirdView(applicationContext, this));
+		customersGirdView = new CustomersGirdView(applicationContext, this);
+		tab1.addComponent(customersGirdView);
 		//tab1.setMargin(false);
 		tab1.setCaption("Customers");
 		tab1.setIcon(VaadinIcons.AIRPLANE);
@@ -32,13 +32,13 @@ public class TabCustomer extends TabSheet {
 		addTab(new CustomerFormView(applicationContext), "Customer", null, 1);
 		
 		setSizeFull();
-	}
-
-	public Customer getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
+		
+		this.addSelectedTabChangeListener(selectedTab -> {
+			TabSheet tabsheet = selectedTab.getTabSheet();
+			Tab tab = tabsheet.getTab(0);
+			if (tab.getCaption().equals("Customers")) {
+				replaceComponent(customersGirdView, new CustomersGirdView(applicationContext, this));
+			}
+		});
 	}
 }
