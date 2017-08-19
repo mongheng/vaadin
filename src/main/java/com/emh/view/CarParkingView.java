272c3eab21daf1291660818af2147fdcc7e8852f
e901 <e901@e901-PC>
2017-08-19 12:03:28 +0700

    commit

--- src/main/java/com/emh/view/CarParkingView.java
+++ src/main/java/com/emh/view/CarParkingView.java
@@ -8,6 +8,8 @@ import org.springframework.context.ApplicationContext;
 import com.emh.model.CarParking;
 import com.emh.model.Customer;
 import com.emh.repository.business.ClassBusiness;
+import com.vaadin.data.Binder;
+import com.vaadin.data.converter.StringToFloatConverter;
 import com.vaadin.data.provider.ListDataProvider;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
@@ -17,6 +19,7 @@ import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Grid.Column;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
@@ -29,6 +32,7 @@ public class CarParkingView extends VerticalLayout {
 	private Customer customer;
 	private List<CarParking> currectCarParkingCustomers;
 	private ListDataProvider<CarParking> dataProvider;
+	private Binder<CarParking> binder;
 
 	private FormLayout formLayout;
 	private HorizontalLayout hLayout;
@@ -51,26 +55,48 @@ public class CarParkingView extends VerticalLayout {
 		hLayout = new HorizontalLayout();
 		grid = new Grid<>();
 
+		binder = new Binder<>();
 		classBusiness = (ClassBusiness) applicationContext.getBean(ClassBusiness.class.getSimpleName());
 
 		cboCustomer = new ComboBox<>("Please Select Customer :");
+		binder.bind(cboCustomer, CarParking::getCustomer, CarParking::setCustomer);
+
 		carTypeField = new TextField("Car Type :");
+		binder.forField(carTypeField).withValidator(carType -> carType != null, "Please input the car type.")
+				.bind(CarParking::getCarType, CarParking::setCarType);
+
 		plantNumberField = new TextField("Plant Number :");
+		binder.forField(plantNumberField)
+				.withValidator(plantNumber -> plantNumber != null, "Please input the Plant Number.")
+				.bind(CarParking::getPlantNumber, CarParking::setPlantNumber);
+
 		amountField = new TextField("Amount :");
+		binder.forField(amountField).withConverter(new StringToFloatConverter("Please input number."))
+				.bind(CarParking::getAmount, CarParking::setAmount);
 
 		ckbFree = new CheckBox("Free");
 		ckbFree.setValue(true);
+		amountField.setEnabled(false);
+		binder.bind(ckbFree, CarParking::isFree, CarParking::setFree);
 		ckbFree.addValueChangeListener(valueChange -> {
 			if (!ckbFree.getValue()) {
 				amountField.clear();
-				amountField.setEnabled(false);
-			} else {
 				amountField.setEnabled(true);
+			} else {
+				amountField.setEnabled(false);
 			}
 		});
 
 		btnSave = new Button("Save");
 		btnSave.addStyleName(ValoTheme.BUTTON_PRIMARY);
+		btnSave.addClickListener(clickEvent -> {
+			CarParking carParking = new CarParking();
+			binder.writeBeanIfValid(carParking);
+			classBusiness.createEntity(carParking);
+			dataProvider.getItems().add(carParking);
+			grid.setDataProvider(dataProvider);
+			Notification.show("The data save successfully.");
+		});
 
 		btnNew = new Button("New");
 		btnNew.addStyleName(ValoTheme.BUTTON_FRIENDLY);
@@ -83,11 +109,12 @@ public class CarParkingView extends VerticalLayout {
 		hLayout.addComponents(btnSave, btnNew);
 		hLayout.setComponentAlignment(btnSave, Alignment.MIDDLE_CENTER);
 		formLayout.addComponents(cboCustomer, carTypeField, plantNumberField, amountField, ckbFree, hLayout);
-		formLayout.setSpacing(false);
-		
+
 		initColumnGrid();
-		grid.setSizeFull();
+		grid.setWidth("100%");
+		grid.setHeight("220px");
 		addComponents(formLayout, grid);
+		setComponentAlignment(grid, Alignment.BOTTOM_LEFT);
 		setCaption("Car Parking");
 		setSizeFull();
 		setSpacing(false);
@@ -112,6 +139,14 @@ public class CarParkingView extends VerticalLayout {
 					: new ListDataProvider<CarParking>(new ArrayList<CarParking>());
 
 			grid.setDataProvider(dataProvider);
+
+			if (currectCarParkingCustomers.size() > 0) {
+				ckbFree.setValue(false);
+				amountField.setEnabled(true);
+			} else {
+				ckbFree.setValue(true);
+				amountField.setEnabled(false);
+			}
 		});
 	}
 
@@ -127,7 +162,9 @@ public class CarParkingView extends VerticalLayout {
 		Column<CarParking, String> columnPlantNumber = grid.addColumn(carParking -> carParking.getPlantNumber());
 		columnPlantNumber.setCaption("Plant Number");
 
-		Column<CarParking, Float> columnAmount = grid.addColumn(CarParking::getAmount);
+		Column<CarParking, String> columnAmount = grid.addColumn(carParking -> {
+			return carParking.getAmount() == null ? "free" : carParking.getAmount().toString();
+		});
 		columnAmount.setCaption("Amount");
 	}
 
